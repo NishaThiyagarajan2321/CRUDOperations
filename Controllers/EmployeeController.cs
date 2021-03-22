d2ce92e65dc8ff8fc04f943d4dc13c95a1e3b023
Nisha Thiyagarajan <nishatl@outlook.com>
2021-03-22 20:10:53 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using CRUDOperations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDOperations.Controllers
+{
+    public class EmployeeController : Controller
+    {
+         
+         Employee emp = new Employee();
+        public static List<Employee> employees = Employee.getAllEmployee();
+        // GET: EmployeeController
+        public ActionResult EmployeeDetails()
+        {
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee emp1 = employees.Where(s => s.EmpId == id).FirstOrDefault();
+            return View(emp1);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+               
+                    emp.Addemployee(employee);
+                    return RedirectToAction("EmployeeDetails");
+            }
+            catch(Exception)
+            {
+                return View("Error");
+            }
+            return View();
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Employee emp1 = employees.Where(s => s.EmpId == id).FirstOrDefault();
+
+            return View(emp1);
+          
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id,Employee employee)
+        {
+            try
+            {
+                Employee emp1 = employees.Where(s => s.EmpId == id).FirstOrDefault();
+                emp1.EmpId = employee.EmpId;
+                emp1.Empname = employee.Empname;
+                emp1.Salary = employee.Salary;
+                return RedirectToAction("EmployeeDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee emp1 = employees.Where(s => s.EmpId == id).FirstOrDefault();
+            return View(emp1);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Employee employee)
+        {
+            try
+            {
+                Employee emp1 = employees.Where(s => s.EmpId == id).FirstOrDefault();
+                employees.Remove(emp1);
+                return RedirectToAction("EmployeeDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDOperations.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set;}
+        public string Empname { get; set; }
+        public float Salary { get; set; }
+        public Employee() { }
+        public Employee (int eid,string empname,float salary)
+        {
+            EmpId = eid;
+            Empname = empname;
+            Salary = salary;
+        }
+        public static List<Employee> employees = new List<Employee>();
+        public static List<Employee> getAllEmployee()
+        {
+         
+            employees.Add(new Employee(100, "Nisha", 55000));
+            employees.Add(new Employee(101, "Vijay", 45000));
+            employees.Add(new Employee(102, "Ramu", 65000));
+            employees.Add(new Employee(103, "Somu", 50000));
+            return employees;
+        }
+        public void Addemployee(Employee employee)
+        {
+            employees.Add(employee);
+        }
+    }
+}
